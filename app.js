7b66400481fc00397e2fb05fda92b9173786592f
Franky Ni <franky.chenxni@gmail.com>
2015-02-16 00:33:50 -0500

    fiddling with directives

--- app.js
+++ app.js
@@ -9,13 +9,40 @@
 			name: 'Dodecahedron',
 			price: 2.95,
 			description: '. . .',
+			reviews: [
+				{
+					stars: 5,
+					body: "I love this product!",
+					author: "joe@thomas.com"
+				},
+				{
+					stars: 1,
+					body: "This product sucks!",
+					author: "tim@hater.com"
+				}
+			],
+			specifications: [],
 			canPurchase: false,
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 5.95,
 			description: '. . .',
+			reviews: [],
+			specifications: [],
 			canPurchase: true,
 		}
 	];
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab
+		}
+	});
 })();
